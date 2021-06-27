7212c2ab243d967c2cec698fc20dabaec5d15633
MUHARREM SERVET ANKARALI <mservetankarali@gmail.com>
2021-06-27 21:10:53 +0300

    add catalog context seed class

--- src/Services/Catalog/Catalog.Api/Dal/CatalogContext.cs
+++ src/Services/Catalog/Catalog.Api/Dal/CatalogContext.cs
@@ -17,6 +17,7 @@ namespace Catalog.Api.Dal
             var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
             
             Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+            CatalogContextSeed.SeedData(Products);
         }
         public IMongoCollection<Product> Products { get; }
     }
