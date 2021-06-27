123e6eaa10baa99f2d291d1292d57ff6190321ef
MUHARREM SERVET ANKARALI <mservetankarali@gmail.com>
2021-06-27 21:05:22 +0300

    add catalog context and set configuration

---
+++ src/Services/Catalog/Catalog.Api/Dal/CatalogContext.cs
@@ -0,0 +1,23 @@
+﻿using Catalog.Api.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Api.Dal
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+            
+            Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
